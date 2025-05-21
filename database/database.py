355575532ba7c3f861052d7838a82eb935155793
oh7895 <oh7895@hanyang.ac.kr>
2025-05-21 13:58:21 +0900

    FIX: DB URL env

--- database/database.py
+++ database/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://cochat:zMz6J5OAuYtExPJP71EFjtS1mJU7v1MM@dpg-d0mlg33e5dus738k8rq0-a.oregon-postgres.render.com/fastapi_cochat_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL
