1f8d1127646ef5b7348139b61b073dfa3c6f34d3
oh7895 <oh7895@hanyang.ac.kr>
2025-05-23 15:51:36 +0900

    FIX: FCM Settings

--- fcm/fcm.py
+++ fcm/fcm.py
@@ -2,8 +2,20 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 import os
 
-cred_path = os.path.join(os.path.dirname(__file__), "../google-services.json")
-cred = credentials.Certificate(cred_path)
+# JSON 구성
+firebase_config = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # 중요!
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+}
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 
 def send_fcm_push(token, title, body):
