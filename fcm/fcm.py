cd35d95bba7bab8a71bc2931266de6dc67116713
oh7895 <oh7895@hanyang.ac.kr>
2025-05-23 12:24:47 +0900

    FIX: Redirect json file

--- fcm/fcm.py
+++ fcm/fcm.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase-service-account.json")
+cred = credentials.Certificate("../firebase-service-account.json")
 firebase_admin.initialize_app(cred)
 
 def send_fcm_push(token, title, body):
