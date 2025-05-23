cdabcf0d75042d7fbde6f70f58195691f7750c92
oh7895 <oh7895@hanyang.ac.kr>
2025-05-23 15:40:43 +0900

    FIX: File Name Changed

--- fcm/fcm.py
+++ fcm/fcm.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
+import os
 
-cred = credentials.Certificate("../firebase-service-account.json")
+cred_path = os.path.join(os.path.dirname(__file__), "../google-services.json")
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 def send_fcm_push(token, title, body):
